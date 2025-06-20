2a8fa40d548e244b7b3c6edcaa8160fdb4bc7317
Leonardo <leolopesquermes1997@gmail.com>
2025-06-20 00:45:36 -0300

    Implementando routes e mapeando components

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import ProdutoList from './components/ProdutoList.vue'
+import ProdutoForm from './components/ProdutoForm.vue'
+import MovimentoList from './components/MovimentoList.vue'
+import MovimentoForm from './components/MovimentoForm.vue'
+
+const routes = [
+  { path: '/', component: ProdutoList },
+  { path: '/produtos/new', component: ProdutoForm },
+  { path: '/produtos/edit/:id', component: ProdutoForm, props: true },
+  { path: '/movimentos', component: MovimentoList },
+  { path: '/movimentos/new', component: MovimentoForm },
+]
+
 const router = createRouter({
   history: createWebHistory(),
   routes,
